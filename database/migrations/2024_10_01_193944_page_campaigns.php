b6011683e45ffb064c2b3366b51d3f6dc064df46
root43242 <hackeandocasas@outlook.com>
2024-10-01 23:48:40 -0300

    Changes

--- database/migrations/2024_10_01_193944_page_campaigns.php
+++ database/migrations/2024_10_01_193944_page_campaigns.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('page_campaigns', function (Blueprint $table) {
             $table->id();
             $table->string('page_id')->unique(); // Armazena o ID da página
-            $table->timestamp('last_activity_campaign')->nullable(); // Data e hora da última campanha
+            $table->string('last_activity_campaign')->nullable(); // Data e hora da última campanha
             $table->timestamps(); // Campos created_at e updated_at
         });
     }
